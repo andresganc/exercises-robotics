1268ee2cc774a4beaf1683dfa378e2506c000a1d
Andres Giraldo <andresganc@gmail.com>
2020-11-24 17:34:11 -0500

    New: Structuring

---
+++ documentation/types/types-modular/index.js
@@ -0,0 +1,8 @@
+
+// ./graphql/typeDefs.js
+import path from 'path';
+import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
+
+const typesArray = fileLoader(path.join(__dirname, './'));
+
+export default mergeTypes(typesArray, { all: true });
