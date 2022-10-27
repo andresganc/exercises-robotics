fd1bad4d636cc104cb14e816220f6b3d3c219256
Andres Giraldo Arenas <andresganc@gmail.com>
2022-10-27 15:23:35 -0500

    Clear repository

--- examples/api-platzi-sqlite/schema/Comentario.js
+++
@@ -1,7 +0,0 @@
-module.exports = `
-  type Comentario {
-    id: ID!
-    nombre: String!
-    cuerpo: String!
-  }
-`
--- exercises/api-newsoft-computer/src/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-
-// ./graphql/typeDefs.js
-import path from 'path';
-import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
-
-const typesArray = fileLoader(path.join(__dirname, './'));
-
-export default mergeResolvers(typesArray, { all: true });
